ba10f18c855299a6a639d2b7c486c7e89216811d
TheGrever1 <104456386+TheGrever1@users.noreply.github.com>
2022-05-12 19:19:40 +0200

    Add files via upload

---
+++ Joysticks/main.py
@@ -0,0 +1,48 @@
+import os
+import time
+from pyjoystick.sdl2 import Key, Joystick, run_event_loop
+from tinydb import TinyDB, Query
+
+db = TinyDB("db.json")
+Bier = Query()
+
+
+def print_add(joy):
+    print("Added", joy)
+
+
+def print_remove(joy):
+    print("Removed", joy)
+
+
+def countup(name: str):
+    data = db.get(Bier.name == name)
+    value = data["quantity"]
+    db.update({"quantity": value + 1}, Bier.name == name)
+
+
+def key_received(key):
+
+    if key == "Button 0" and key.value == 1:  # Grün Urplis
+        countup("Karlsberg")
+        countup("Insgesamt")  # hier drin da es sonst 2x zählt zu faul für fix
+
+    if key == "Button 1" and key.value == 1:  # Bitburger Gelb mit Zettel
+        countup("Bitburger")
+        countup("Insgesamt")
+
+    if key == "Button 2" and key.value == 1:  # Radler Gelb ohne Zettel
+        countup("Radler")
+        countup("Insgesamt")
+
+    if key == "Button 3" and key.value == 1:  # AlkFrei Blau
+        countup("AlkFrei")
+        countup("Insgesamt")
+
+    if key == "Button 4" and key.value == 1:  # Rot Emergency
+        print(key)
+
+    time.sleep(0.5)
+
+
+run_event_loop(print_add, print_remove, key_received)
